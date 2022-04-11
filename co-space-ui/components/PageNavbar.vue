a42370fa8365ba9bd1ff87a352d29ed60b6e9f8c
Nattawat Hayamin <INatZaza@hotmail.com>
2022-04-11 15:13:28 +0700

    changePage

---
+++ co-space-ui/components/PageNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav
+      class="navbar header navbar-color-green"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/main">
+          <font-awesome-icon icon="home" size="2x" class="" />
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.navbar-color-green {
+  background-color: #a1c7b7;
+}
+</style>
---
+++ co-space-ui/pages/foodPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <PageNavbar />
+    <section class="section is-small">
+      <div class="columns">
+        <div class="column is-3">
+          <div class="card">
+            <div class="card-content">
+              <div class="content has-text-centered"></div>
+              <div class=""></div>
+            </div>
+            <footer class="card-footer">
+              <div class="card-content">
+                <div class="content has-text-centered"></div>
+                <div class="">test</div>
+              </div>
+            </footer>
+          </div>
+        </div>
+
+        <div class="column is-9">
+          <div class="card">
+            <div class="card-content">
+              <div class="content has-text-centered"></div>
+              <div class="">
+                <h1 class="title has-text-centered color-blue">test</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'foodPage',
+  components: {},
+}
+</script>
+
+<style lang="css" scope>
+.full-height {
+  height: 100vh;
+}
+
+.card-footer {
+  flex-direction: column;
+  align-content: center;
+  flex-wrap: wrap;
+}
+</style>
