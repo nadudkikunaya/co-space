34b22c682bc1a80ded6d623364b5ee7efe81d4ac
Nattawat Hayamin <INatZaza@hotmail.com>
2022-04-12 19:58:07 +0700

    test

--- co-space-api/routes/food.js
+++ co-space-api/routes/food.js
@@ -21,4 +21,25 @@ router.get("/foodlist", async (req, res) => {
     conn.release();
   }
 });
+
+router.get("/member", async (req, res) => {
+  const conn = await pool.getConnection();
+  await conn.beginTransaction();
+  try {
+    sql = `SELECT * FROM members`;
+    const [data] = await conn.query(sql);
+
+    return res.json({
+      success: true,
+      data: data,
+    });
+  } catch (err) {
+    console.log(err);
+    conn.rollback();
+    res.status(400).json(err.toString());
+  } finally {
+    conn.release();
+  }
+});
+
 module.exports = router;
--- co-space-ui/pages/main.vue
+++ co-space-ui/pages/main.vue
@@ -4,21 +4,23 @@
       class="columns is-variable is-2-mobile is-0-tablet is-5-desktop is-5-widescreen is-5-fuulhd"
     >
       <div class="column">
-        <FoodCard title="Food/Beverage" icon="utensils" to-page="foodpage">
-          Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-        </FoodCard>
+        <MenuCard
+          title="Food/Beverage"
+          icon="utensils"
+          to-page="foodpage"
+        ></MenuCard>
       </div>
       <div class="column is-1"></div>
       <div class="column">
-        <FoodCard title="Books" icon="book" to-page="booksPage">
-          Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-        </FoodCard>
+        <MenuCard title="Books" icon="book" to-page="booksPage"></MenuCard>
       </div>
       <div class="column is-1"></div>
       <div class="column">
-        <FoodCard title="Member" icon="user-large" to-page="memberPage">
-          Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-        </FoodCard>
+        <MenuCard
+          title="Member"
+          icon="user-large"
+          to-page="memberPage"
+        ></MenuCard>
       </div>
     </div>
 
@@ -27,15 +29,15 @@
     >
       <div class="column is-2"></div>
       <div class="column">
-        <FoodCard title="Report" icon="chart-pie" to-page="reportPage">
-          Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-        </FoodCard>
+        <MenuCard
+          title="Report"
+          icon="chart-pie"
+          to-page="reportPage"
+        ></MenuCard>
       </div>
       <div class="column is-1"></div>
       <div class="column">
-        <FoodCard title="Setting" icon="gear" to-page="settingPage">
-          Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-        </FoodCard>
+        <MenuCard title="Setting" icon="gear" to-page="settingPage"></MenuCard>
       </div>
       <div class="column is-2"></div>
     </div>
@@ -45,7 +47,7 @@
 export default {
   name: 'MainPage',
   components: {
-    FoodCard: () => import('@/components/FoodCard'),
+    MenuCard: () => import('@/components/MenuCard'),
   },
   data() {
     return {}
