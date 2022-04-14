56ff3af6fac2a88f7fe36383f98d45fd8ced0546
leoevi <chopang_123@hotmail.com>
2022-04-14 15:03:56 +0700

    add: member.js

--- co-space-api/routes/index.js
+++ co-space-api/routes/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const food = require("./food");
+const member = require("./member");
 const krit = require("./krit");
 const chopang = require("./chopang");
 
-router.use(food);
+// test
 router.use(krit);
 router.use(chopang);
+
+// production
+router.use(food);
+router.use(member);
 
 module.exports = router;
