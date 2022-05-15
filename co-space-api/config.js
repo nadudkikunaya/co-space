0df391ce134d225693272974142346967c73ae6e
leoevi <chopang_123@hotmail.com>
2022-05-15 19:43:29 +0700

    gitignore test

--- co-space-api/config.js
+++ co-space-api/config.js
@@ -15,7 +15,7 @@ const moment = require("moment-timezone");
 module.exports.pool = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  password: "",
+  password: "random password lmao",
   database: "library",
   timezone: "Asia/Bangkok",
   waitForConnections: true,
