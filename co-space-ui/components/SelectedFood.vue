d09438b39b8085fec01255f20b5d4c75f19e8082
Nattawat Hayamin <INatZaza@hotmail.com>
2022-05-15 19:09:46 +0700

    new update

---
+++ co-space-ui/components/SelectedFood.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="columns">
+        <div class="column is-4">
+          <p>{{ name }}</p>
+        </div>
+        <div class="column is-4">
+          <p>{{ calc_price }}</p>
+        </div>
+        <div class="column is-4">
+          <button @click="increase()">+</button>
+          {{ qnt }}
+          <button @click="decrease()">-</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectedFood',
+  data() {
+    return {
+      qnt: this.quantity,
+      base_price: this.price,
+      calc_price: this.price,
+    }
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    increase() {
+      this.qnt++
+      this.calc_price = this.base_price * this.qnt
+      // console.log(this.qnt)
+      this.$emit('updateQuantity', this.name, this.qnt)
+    },
+    decrease() {
+      this.qnt--
+      // console.log(this.qnt)
+      this.calc_price = this.base_price * this.qnt
+      this.$emit('updateQuantity', this.name, this.qnt)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ co-space-ui/pages/test.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="section is-large">
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          <h1 class="title has-text-centered">Login system</h1>
+          <center>
+            <b-field label="Name" :label-position="labelPosition">
+              <b-input value="Kevin Garvey"></b-input>
+            </b-field>
+
+            <b-field label="Password" :label-position="labelPosition">
+              <b-input value="Kevin Garvey"></b-input>
+            </b-field>
+
+            <b-button type="is-info">Login</b-button>
+          </center>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TestPage',
+  data() {
+    return {
+      expandOnHover: false,
+      expandWithDelay: false,
+      mobile: 'reduce',
+      reduce: false,
+    }
+  },
+}
+</script>
