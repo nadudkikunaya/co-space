11e34147355943f25afb13f76a0c1b3a4b57f597
Nattawat Hayamin <INatZaza@hotmail.com>
2022-05-15 19:49:14 +0700

    test

--- co-space-api/config.js
+++ co-space-api/config.js
@@ -15,7 +15,7 @@ const moment = require("moment-timezone");
 module.exports.pool = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  password: "random test ssstttt",
+  password: "random testsadasd ssstttt",
   database: "library",
   timezone: "Asia/Bangkok",
   waitForConnections: true,
