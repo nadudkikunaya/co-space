2c38bfd63dccc7cb6aa33c30f250757cf5790e56
Nattawat Hayamin <INatZaza@hotmail.com>
2022-05-16 23:10:45 +0700

    change index page

--- co-space-ui/pages/index.vue
+++ co-space-ui/pages/index.vue
@@ -1,33 +1,58 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github">
-        Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
-      </card>
+  <section class="section is-medium">
+    <div
+      class="columns is-variable is-2-mobile is-0-tablet is-5-desktop is-5-widescreen is-5-fullhd"
+    >
+      <div class="column">
+        <MenuCard
+          title="Food/Beverage"
+          icon="utensils"
+          to-page="foodpage"
+        ></MenuCard>
+      </div>
+      <div class="column is-1"></div>
+      <div class="column">
+        <MenuCard title="Ayylmao" icon="book" to-page="booksPage"></MenuCard>
+      </div>
+      <div class="column is-1"></div>
+      <div class="column">
+        <MenuCard
+          title="Member"
+          icon="user-large"
+          to-page="memberPage"
+        ></MenuCard>
+      </div>
+    </div>
 
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey"> Every </b> component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with <a href="https://vuejs.org/"> Vue.js </a> and
-        <a href="http://bulma.io/"> Bulma </a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
+    <div
+      class="columns is-variable is-2-mobile is-0-tablet is-8-desktop is-8-widescreen is-8-fuulhd"
+    >
+      <div class="column is-2"></div>
+      <div class="column">
+        <MenuCard
+          title="Report"
+          icon="chart-pie"
+          to-page="reportPage"
+        ></MenuCard>
+      </div>
+      <div class="column is-1"></div>
+      <div class="column">
+        <MenuCard title="Setting" icon="gear" to-page="settingPage"></MenuCard>
+      </div>
+      <div class="column is-2"></div>
     </div>
   </section>
 </template>
-
 <script>
-import Card from '~/components/Card'
-
 export default {
-  name: 'IndexPage',
+  name: 'MainPage',
   components: {
-    Card,
+    MenuCard: () => import('@/components/MenuCard'),
   },
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
 }
 </script>
